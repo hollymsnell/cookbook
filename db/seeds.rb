c88fbd943e05724cdc4aef738177481b750ecaf7
hollymsnell <hollymsnell@gmail.com>
2022-09-28 21:23:31 -0600

    added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-recipes = Recipe.create(name: "Tomato Cucumber Salad", instructions: "")
+recipes = Recipe.create!([
+{name: "Tomato Cucumber Salad", ingredients: "One english cucumber, 16oz tomato, shallot, feta cheese, garbanzo beans, lemon juice, dill, salt", instructions: "cut tomatoes, shallot, and cucumber and add everything together", prep_minutes: 20, cook_minutes: 0},
+{name: "White Bean Chili", ingredients: "white beans, turkey/chicken, green chili, broth", instructions: "add it all together and cook in crockpot", prep_minutes: 15, cook_minutes: 45},
+{name: "Nice Cream", ingredients: "bananas", instructions: "put frozen bananas through blender and blend until creamy", prep_minutes: 5, cook_minutes: 0}
+])
