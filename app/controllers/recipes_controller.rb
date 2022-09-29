94015b2cb9dd353514e79e8be7f2fb30acd056ff
hollymsnell <hollymsnell@gmail.com>
2022-09-28 21:48:03 -0600

    5 RESTful API actions in recipe controller for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,42 @@
 class RecipesController < ApplicationController
+
+  def index
+    recipes = Recipe.all
+    render json:recipes.as_json
+  end
+
+  def create
+    recipe = Recipe.new(
+      name: params[:name],
+      ingredients: params[:ingredients],
+      instructions: params[:instructions],
+      prep_minutes: params[:prep_minutes],
+      cook_minutes: params[:cook_minutes]
+    )
+    recipe.save
+    render json: recipe.as_json
+  end
+
+  def show
+    recipe = Recipe.find_by(id: params[:id])
+    render json: recipe.as_json
+  end
+
+  def update
+    recipe = Recipe.find_by(id: params[:id])
+    recipe.name = params[:name] || recipe.name
+    recipe.ingredients = params[:ingredients] || recipe.ingredients
+    recipe.instructions = params[:instructions] || recipe.instructions
+    recipe.prep_minutes = params[:prep_minutes] || recipe.prep_minutes
+    recipe.cook_minutes = params[:cook_minutes] || recipe.cook_minutes
+    recipe.save
+    render json: recipe.as_json
+  end
+
+  def destroy
+    recipe = Recipe.find_by(id: params[:id])
+    recipe.destroy
+    render json: {message: "Recipe Vanished."}
+  end
+
 end
